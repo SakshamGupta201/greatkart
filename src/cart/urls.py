7b398a5fcf58734c535e827b33c67bd7bf2e207e
Saksham Gupta <guptasaksham495@gmail.com>
2024-10-20 01:12:37 +0530

    Refactor cart module and add update cart item quantity functionality

--- src/cart/urls.py
+++ src/cart/urls.py
@@ -6,4 +6,9 @@ app_name = "cart"
 urlpatterns = [
     path("", views.cart_view, name="cart"),
     path("add/<slug:product_slug>/", views.add_to_cart, name="add_to_cart"),
+    path(
+        "update_quantity/<int:item_id>/",
+        views.update_cart_item_quantity,
+        name="update_cart_item_quantity",
+    ),
 ]
--- src/cart/views.py
+++ src/cart/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.shortcuts import get_object_or_404
 from Product.models import Product
@@ -6,8 +7,17 @@ from cart.models import Cart, CartItem
 
 def cart_view(request):
     cart_items = CartItem.objects.filter(cart__cart_id=_get_cart_id(request))
+    total_price = float(
+        sum(cart_item.product.price * cart_item.quantity for cart_item in cart_items)
+    )
+    tax = round(0.1 * total_price, 2)
+    grand_total = round(total_price + tax, 2)
+
     context = {
         "cart_items": cart_items,
+        "total_price": round(total_price, 2),
+        "tax": tax,
+        "grand_total": grand_total,
     }
     return render(request, "cart.html", context)
 
@@ -31,4 +41,39 @@ def add_to_cart(request, product_slug):
         cart_item.quantity += 1
     cart_item.save()
 
-    return redirect("cart_view")
+    return redirect("cart:cart")
+
+
+def update_cart_item_quantity(request, item_id):
+    if request.method == "POST":
+        cart_item = get_object_or_404(CartItem, id=item_id)
+        quantity = int(request.POST.get("quantity"))
+        cart_item.quantity = quantity
+        cart_item.save()
+        sub_total = cart_item.sub_total()
+        total = float(
+            sum(
+                cart_item.product.price * cart_item.quantity
+                for cart_item in CartItem.objects.filter(
+                    cart__cart_id=_get_cart_id(request)
+                )
+            )
+        )
+        return JsonResponse(
+            {
+                "success": True,
+                "sub_total": sub_total,
+                "total": total,
+            }
+        )
+
+    else:
+        return JsonResponse(
+            {"success": False, "error": "Invalid request method"}, status=400
+        )
+
+
+def remove_cart_item(request, item_id):
+    cart_item = get_object_or_404(CartItem, id=item_id)
+    cart_item.delete()
+    return redirect("cart:cart")
