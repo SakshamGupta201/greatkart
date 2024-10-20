78cf4bb04cae13f6310d515cbf12535dd2a3baa7
Saksham Gupta <guptasaksham495@gmail.com>
2024-10-20 12:56:46 +0530

    Refactor store views to implement pagination for products

--- src/store/views.py
+++ src/store/views.py
@@ -1,29 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from Product.models import Product
 from category.models import Category
 
 
-def store_view(request):
+def paginate_products(request, products_list):
+    products_list = products_list.order_by("slug")
+    paginator = Paginator(products_list, 10)
+    page = request.GET.get("page")
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+    return products
 
-    products = Product.objects.all()
+
+def store_view(request):
+    products_list = Product.objects.all()
+    products = paginate_products(request, products_list)
+
     context = {
         "products": products,
-        "count": products.count(),
+        "count": products_list.count(),
     }
     return render(request, "store.html", context)
 
 
 def category_view(request, category_slug):
-    try:
-        category = Category.objects.get(slug=category_slug)
-        products = Product.objects.filter(category=category)
-    except Category.DoesNotExist:
-        category = None
-        products = Product.objects.none()
+    category = get_object_or_404(Category, slug=category_slug)
+    products_list = Product.objects.filter(category=category)
+    products = paginate_products(request, products_list)
 
     context = {
         "products": products,
-        "count": products.count(),
+        "count": products_list.count(),
     }
     return render(request, "store.html", context)
