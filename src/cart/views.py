4ff3c3ee752fbe7a4e012535e6cef54c4e8cda8f
Saksham Gupta <guptasaksham495@gmail.com>
2024-10-24 14:51:49 +0530

    Refactor cart module to add support for product variations and improve cart item handling

--- src/cart/views.py
+++ src/cart/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import redirect, render, get_object_or_404
 from Product.models import Product, Variation
 from cart.models import Cart, CartItem
+from django.db.models import Q
 
 
 def cart_view(request):
@@ -31,33 +32,56 @@ def _calculate_totals(cart_items):
     return total_price, tax, grand_total
 
 
+from django.db.models import Count, Q
+
+
 def add_to_cart(request, product_slug):
     product = get_object_or_404(Product, slug=product_slug)
     variation_products = []
+
     if request.method == "POST":
-        for key, value in request.POST.items():
-            print(f"{key}: {value}")
-            try:
-                variation = Variation.objects.get(
-                    product=product, category__iexact=key, value=value
-                )
-                variation_products.append(variation)
-            except Variation.DoesNotExist:
-                continue
+        variation_products = [
+            Variation.objects.get(
+                product=product, category__iexact=key, value__iexact=value
+            )
+            for key, value in request.POST.items()
+            if Variation.objects.filter(
+                product=product, category__iexact=key, value__iexact=value
+            ).exists()
+        ]
 
     cart_id = _get_cart_id(request)
     cart, _ = Cart.objects.get_or_create(cart_id=cart_id)
 
-    cart_item, created = CartItem.objects.get_or_create(product=product, cart=cart)
-    if variation_products:
-        cart_item.variation.clear()
-        cart_item.variation.set(variation_products)
-    if created:
-        cart_item.quantity = 1
-    else:
-        cart_item.quantity += 1
-    cart_item.save()
+    cart_items = CartItem.objects.filter(product=product, cart=cart)
 
+    if variation_products:
+        variations_filter = Q()
+        for variation in variation_products:
+            variations_filter |= Q(variation=variation)
+
+        cart_items = (
+            cart_items.filter(variations_filter)
+            .annotate(variation_count=Count("variation"))
+            .filter(variation_count=len(variation_products))
+        )
+
+        matching_cart_items = [
+            cart_item
+            for cart_item in cart_items
+            if set(cart_item.variation.all()) == set(variation_products)
+        ]
+
+        if matching_cart_items:
+            cart_item = matching_cart_items[0]
+            cart_item.quantity += 1
+        else:
+            cart_item = CartItem.objects.create(product=product, cart=cart, quantity=1)
+            cart_item.variation.set(variation_products)
+    else:
+        cart_item = CartItem.objects.create(product=product, cart=cart, quantity=1)
+
+    cart_item.save()
     return redirect("cart:cart")
 
 
